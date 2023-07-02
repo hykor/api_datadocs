0c2b4cb572e543b26c8422252631a65769e339f8
hykor <41054764+hykor@users.noreply.github.com>
2023-07-02 20:50:14 +0700

    update api

--- application/views/api.php
+++ application/views/api.php
@@ -16,17 +16,43 @@ header("Access-Control-Allow-Methods: GET, OPTIONS");
 
  
 	<script>
-		let require = new XMLHttpRequest();
-		require.open('GET','https://jsonplaceholder.typicode.com/users');
-		require.send();
-		require.onload = () => {
-			console.log(require);
-			if (require.status === 200) {
-				console.log(JSON.parse(require.response))
-			}else {
-				console.log(`error ${request.status} ${request.statusText}`);
-			}
-		} 
+		// Method GET
+		// let require = new XMLHttpRequest();
+		// require.open('GET','https://jsonplaceholder.typicode.com/users');
+		// require.send();
+		// require.onload = () => {
+		// 	console.log(require);
+		// 	if (require.status === 200) {
+		// 		console.log(JSON.parse(require.response))
+		// 	}else {
+		// 		console.log(`error ${request.status} ${request.statusText}`);
+		// 	}
+		// } 
+
+		// Method POST
+		// let require = new XMLHttpRequest();
+		// require.open("POST", "http://localhost:3000/users");
+		// require.setRequestHeader("Content-Type", "application/json");
+		// let data = `{
+		// 	"id": 12,
+		// 	"first_name": "test",
+		// 	"last_name": "test",
+		// 	"email": "hildegard@hotmail.com",
+		// }`
+		// require.onload = () => console.log(require.responseText);
+		// require.send(data);
+
+		fetch('http://localhost:3000/users', {
+    	method: 'POST',
+    	headers: {
+    	    'Accept': 'application/json',
+    	    'Content-Type': 'application/json'
+    	},
+    	body: JSON.stringify({ "id": 12 })
+		})
+   		.then(response => response.json())
+   		.then(response => console.log(JSON.stringify(response)))
+
 	</script>
 
 </html>
